6760f6a74a16f34b0a606b44beda196a8348a208
Tom Brereton <Tom.Brereton@asos.com>
2018-09-09 18:50:03 +0100

    Remove String Calc class from Test Class

--- CSharpStringCalculator/StringCalculatorTests/StringCalculatorShould.cs
+++ CSharpStringCalculator/StringCalculatorTests/StringCalculatorShould.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text.RegularExpressions;
+using CSharpStringCalculator;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 // ReSharper disable PossibleMultipleEnumeration
 
@@ -59,60 +55,6 @@ namespace StringCalculatorTests
 
             var exception = Assert.Throws<ArgumentException>(() => StringCalculator.Sum(numbersWithIncorrectFormat));
             Assert.That(exception.Message, Is.EqualTo(expectedMessage));
-        }
-    }
-
-    public class StringCalculator
-    {
-        public static int Sum(string numbersToAdd)
-        {
-            if (string.IsNullOrEmpty(numbersToAdd))
-            {
-                return 0;
-            }
-
-            var pattern = CreateSeparatorPattern(numbersToAdd);
-            var trimmedNumbers = TrimSeparatorFromString(numbersToAdd);
-            var listOfNumbers = Regex.Split(trimmedNumbers, pattern);
-
-            ThrowArgumentExceptionIfThereAreNegativeNumbers(listOfNumbers);
-
-            return listOfNumbers
-                .Select(int.Parse)
-                .Where(x => x < 1001)
-                .Sum();
-        }
-
-        private static void ThrowArgumentExceptionIfThereAreNegativeNumbers(IEnumerable<string> listOfNumbers)
-        {
-            var badNumbers = listOfNumbers.Where(x => x.Contains("-"));
-            if (badNumbers.Any())
-            {
-                throw new ArgumentException("Negatives not allowed: " + string.Join(",", badNumbers));
-            }
-        }
-
-        private static string TrimSeparatorFromString(string numbersToAdd)
-        {
-            if (numbersToAdd.StartsWith("//"))
-            {
-                var index = numbersToAdd.IndexOf('\n');
-                numbersToAdd = numbersToAdd.Substring(index + 1);
-            }
-
-            return numbersToAdd;
-        }
-
-        private static string CreateSeparatorPattern(string numbersToAdd)
-        {
-            var pattern = "[,\n]+";
-            if (numbersToAdd.StartsWith("//"))
-            {
-                var separator = numbersToAdd.TrimStart('/').Split('\n')[0].Replace("[", "").Replace("]", "");
-                pattern = $"{pattern}|[{separator}]+";
-            }
-
-            return pattern;
         }
     }
 }
